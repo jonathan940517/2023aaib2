9db617bc2277cfd56f5ee95ef2cb580ed71e9812
Jonathan940517 <jonathan940507@gmail.com>
2024-03-15 10:43:32 +0800

    modify

---
+++ week04/week04-3.cpp
@@ -0,0 +1,10 @@
+# include<stdio.h>
+int main(){
+  for(int i=1;i<=9;i++){
+    printf("i is %d\n",i);
+  }
+  printf("\n");
+    for(int i=9;i>0;i--){
+    printf("i is %d\n",i);
+  }
+}
---
+++ week04/week04-3.py
@@ -0,0 +1,5 @@
+for i in range(1,10):
+  print("i is",i)
+print()
+for i in range(9,0,-1):
+  print("i is",i)
